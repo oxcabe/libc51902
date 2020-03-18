8933098b700c174f7a1e460090fee8e09fb91cde
Óscar Carrasco <oxcabe@gmail.com>
2020-03-18 05:33:47 +0000

    Release 0.1.0 version
    Implement current CPU features
    
    Remove unnecesary module definitions
    
    Implement opcode construction
    
    Re-export opcode::Opcode
    
    Refactor opcode implementation
    
    Implement display derive for Opcode enum members

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod flag;
+pub mod opcode;
+pub mod spec;
+
+pub use crate::opcode::Opcode;
---
+++ src/opcode.rs
@@ -0,0 +1,55 @@
+extern crate strum;
+
+use strum_macros::Display;
+
+/// Instruction opcode representation
+#[derive(Display)]
+pub enum Opcode {
+    /// no-operation
+    Nop,
+    /// unconditional jump
+    Jmp,
+    /// conditional jump
+    Jz,
+    /// move data between regs
+    Mov,
+    /// bitwise negation
+    Not,
+    /// add two registers and store in dest
+    Add,
+    /// substract two registers and store in dest
+    Sub,
+    /// bitwise AND
+    And,
+    /// bitwise OR
+    Or,
+    NegH,
+    NegL,
+    /// load immediate into register
+    Li,     
+}
+
+impl Opcode {
+    pub fn op_str(&self) -> String {
+        self.to_string().to_lowercase()
+    }
+
+    // [u8] deallocates in parent scope
+    pub fn to_binary<'a>(&self) -> &'a[u8] {
+        use Opcode::*;
+        match self {
+            Nop     => &[0, 0, 0, 0, 0, 0],
+            Jmp     => &[0, 0, 0, 1, 0, 0],
+            Jz      => &[0, 0, 0, 1, 0, 1],
+            Mov     => &[1, 0, 0, 0],
+            Not     => &[1, 0, 0, 1],
+            Add     => &[1, 0, 1, 0],
+            Sub     => &[1, 0, 1, 1],
+            And     => &[1, 1, 0, 0],
+            Or      => &[1, 1, 0, 1],
+            NegH    => &[1, 1, 1, 0],
+            NegL    => &[1, 1, 1, 1],
+            Li      => &[0, 0, 1, 0],
+        }
+    }
+}
